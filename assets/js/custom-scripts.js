82ae2e023f6cbc9243641247631536874b1f4dab
zionlew <zionlew@163.com>
2022-05-04 20:36:42 +0800

    1

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -8,5 +8,27 @@ function nav() {
 }
 
 function sentence_t() {
-    alert("原文dd");
+    if (document.getElementById("sentence_t").style.display == "block") {
+        document.getElementById("sentence_t").style.display = "none"
+    } else {
+        document.getElementById("sentence_t").style.display = "block"
+    }
+}
+
+function sentence_def() {
+    if (document.getElementById("sentence_def").style.display == "block") {
+        document.getElementById("sentence_def").style.display = "none"
+    } else {
+        document.getElementById("sentence_def").style.display = "block"
+    }
+}
+
+function sentence_next() {
+
+    var maxCnt = 3
+    sid = Math.floor(Math.random() * (maxCnt - 1)) + 1;
+    sid = sid + parseInt(document.getElementById("s_id").value);
+    if (sid > maxCnt) { sid = sid - maxCnt }
+    location.href = "../../riyu/sentence" + sid + ".html";
+
 }
