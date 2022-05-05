9d2b35a791591eb0e6d542c55f770d71bf23f312
zionlew <zionlew@163.com>
2022-05-05 21:42:29 +0800

    1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,6 +20,8 @@
                     $('div.sidebar-collapse').removeClass('collapse')
                 }
             });
+			
+ 
 
             /* MORRIS BAR CHART
 			-----------------------------------------*/
@@ -57,15 +59,9 @@
                 xkey: 'y',
                 ykeys: ['a', 'b'],
                 labels: ['Series A', 'Series B'],
-				 barColors: [
-    '#A6A6A6','#F09B22',
-    '#A8E9DC' 
-  ],
                 hideHover: 'auto',
                 resize: true
             });
-	 
-
 
             /* MORRIS DONUT CHART
 			----------------------------------------*/
@@ -81,10 +77,6 @@
                     label: "Mail-Order Sales",
                     value: 20
                 }],
-				   colors: [
-    '#A6A6A6','#F09B22',
-    '#A8E9DC' 
-  ],
                 resize: true
             });
 
@@ -149,9 +141,6 @@
                 labels: ['iPhone', 'iPad', 'iPod Touch'],
                 pointSize: 2,
                 hideHover: 'auto',
-				  pointFillColors:['#ffffff'],
-				  pointStrokeColors: ['black'],
-				  lineColors:['#A6A6A6','#F09B22'],
                 resize: true
             });
 
@@ -159,42 +148,43 @@
 			----------------------------------------*/
             Morris.Line({
                 element: 'morris-line-chart',
-                data: [
-					  { y: '2014', a: 50, b: 90},
-					  { y: '2015', a: 165,  b: 185},
-					  { y: '2016', a: 150,  b: 130},
-					  { y: '2017', a: 175,  b: 160},
-					  { y: '2018', a: 80,  b: 65},
-					  { y: '2019', a: 90,  b: 70},
-					  { y: '2020', a: 100, b: 125},
-					  { y: '2021', a: 155, b: 175},
-					  { y: '2022', a: 80, b: 85},
-					  { y: '2023', a: 145, b: 155},
-					  { y: '2024', a: 160, b: 195}
-				],
-            
-				 
-      xkey: 'y',
-      ykeys: ['a', 'b'],
-      labels: ['Total Income', 'Total Outcome'],
-      fillOpacity: 0.6,
-      hideHover: 'auto',
-      behaveLikeLine: true,
-      resize: true,
-      pointFillColors:['#ffffff'],
-      pointStrokeColors: ['black'],
-      lineColors:['gray','#F09B22']
-	  
+                data: [{
+                    y: '2006',
+                    a: 100,
+                    b: 90
+                }, {
+                    y: '2007',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2008',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2009',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2010',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2011',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2012',
+                    a: 100,
+                    b: 90
+                }],
+                xkey: 'y',
+                ykeys: ['a', 'b'],
+                labels: ['Series A', 'Series B'],
+                hideHover: 'auto',
+                resize: true
             });
            
-        
-            $('.bar-chart').cssCharts({type:"bar"});
-            $('.donut-chart').cssCharts({type:"donut"}).trigger('show-donut-chart');
-            $('.line-chart').cssCharts({type:"line"});
-
-            $('.pie-thychart').cssCharts({type:"pie"});
-       
-	 
+     
         },
 
         initialization: function () {
@@ -206,20 +196,7 @@
     // Initializing ///
 
     $(document).ready(function () {
-        mainApp.initFunction(); 
-		$("#sideNav").click(function(){
-			if($(this).hasClass('closed')){
-				$('.navbar-side').animate({left: '0px'});
-				$(this).removeClass('closed');
-				$('#page-wrapper').animate({'margin-left' : '260px'});
-				
-			}
-			else{
-			    $(this).addClass('closed');
-				$('.navbar-side').animate({left: '-260px'});
-				$('#page-wrapper').animate({'margin-left' : '0px'}); 
-			}
-		});
+        mainApp.initFunction();
     });
 
 }(jQuery));
